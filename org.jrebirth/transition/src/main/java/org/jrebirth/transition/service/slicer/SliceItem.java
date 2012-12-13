32e71c037a920c9d883fe7edb6365645051f4c20
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2012-12-13 22:08:33 +0100

    Manage Slicer service

---
+++ org.jrebirth/transition/src/main/java/org/jrebirth/transition/service/slicer/SliceItem.java
@@ -0,0 +1,55 @@
+package org.jrebirth.transition.service.slicer;
+
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+
+/**
+ * The class <strong>SliceItem</strong>.
+ * 
+ * @author Sébastien Bordes
+ */
+public class SliceItem {
+
+    /** The node representing the slice. */
+    private Node sliceNode;
+
+    /** The original node bounds. */
+    private Bounds nodeBounds;
+
+    /**
+     * Gets the slice node.
+     * 
+     * @return the slice node
+     */
+    public Node getSliceNode() {
+        return this.sliceNode;
+    }
+
+    /**
+     * Sets the node representing the slice.
+     * 
+     * @param sliceNode the new node representing the slice
+     */
+    public void setSliceNode(final Node sliceNode) {
+        this.sliceNode = sliceNode;
+    }
+
+    /**
+     * Gets the original node bounds.
+     * 
+     * @return the original node bounds
+     */
+    public Bounds getNodeBounds() {
+        return this.nodeBounds;
+    }
+
+    /**
+     * Sets the original node bounds.
+     * 
+     * @param nodeBounds the new original node bounds
+     */
+    public void setNodeBounds(final Bounds nodeBounds) {
+        this.nodeBounds = nodeBounds;
+    }
+
+}
