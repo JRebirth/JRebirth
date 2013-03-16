7f9a41d01029022e7d4143ed252cc13b5a13ed45
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2013-03-16 18:00:37 +0100

    Manage site l&f with a dedicated skin

--- org.jrebirth.site.skin/src/main/resources/js/lightbox.js
+++ org.jrebirth.site.skin/src/main/resources/js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = '../images/lightbox/loading.gif';
+      this.fileCloseImage = '../images/lightbox/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
