48782956ee88de382be38e48b639bff407810922
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2013-08-01 21:15:10 +0200

    Provide a beta StackModel as a reusable component

---
+++ org.jrebirth/component/src/main/java/org/jrebirth/component/ui/stack/PageEnum.java
@@ -0,0 +1,10 @@
+package org.jrebirth.component.ui.stack;
+
+import org.jrebirth.core.key.UniqueKey;
+import org.jrebirth.core.ui.Model;
+
+public interface PageEnum {
+
+    UniqueKey<? extends Model> getModelKey();
+
+}
