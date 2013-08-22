0edfa5a2310de95d41fa6e0d8fff379d18eff690
Bastien <bastien.billey@dooapp.com>
2013-08-22 14:49:47 +0200

    Fix bug on the UndoRedoService

--- org.jrebirth/undoredo/src/main/java/org/jrebirth/undoredo/service/UndoRedoService.java
+++ org.jrebirth/undoredo/src/main/java/org/jrebirth/undoredo/service/UndoRedoService.java
@@ -2,13 +2,13 @@
  * Get more info at : www.jrebirth.org .
  * Copyright JRebirth.org © 2011-2013
  * Contact : sebastien.bordes@jrebirth.org
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,24 +17,23 @@
  */
 package org.jrebirth.undoredo.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.jrebirth.core.service.ServiceBase;
 import org.jrebirth.core.wave.WaveBuilder;
 import org.jrebirth.core.wave.WaveData;
 import org.jrebirth.undoredo.command.UndoRedoWaves;
 import org.jrebirth.undoredo.command.Undoable;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * The Class UndoRedoService is used to manage a stack of {@link Undoable} command.
- * 
+ *
  * It must have an unique name if several stack are managed together. (Use a basic JRebirth component key)
- * 
+ *
  * @author Sébastien Bordes
  */
 public class UndoRedoService extends ServiceBase {
@@ -42,17 +41,19 @@ public class UndoRedoService extends ServiceBase {
     /** The class logger. */
     private static final Logger LOGGER = LoggerFactory.getLogger(UndoRedoService.class);
 
-    /** The current index of the task performed. */
-    private int currentIndex;
-
     /** The command stack. */
     private final List<Undoable> commandStack = Collections.synchronizedList(new ArrayList<Undoable>());
 
     /**
+     * The commands that have been undone.
+     */
+    private final List<Undoable> undoneStack = Collections.synchronizedList(new ArrayList<Undoable>());
+
+    /**
      * Stack up a command.
-     * 
+     *
      * Move internal index and execute the command by triggering a Redo Wave Command
-     * 
+     *
      * @param command the command
      */
     public void stackUp(final Undoable command) {
@@ -60,11 +61,8 @@ public class UndoRedoService extends ServiceBase {
         // Stack up the command
         this.commandStack.add(command);
 
-        // Move forward the command cursor
-        this.currentIndex++;
-
         // Call the redo method of the Undoable command
-        this.commandStack.get(this.currentIndex - 1).run(
+        this.commandStack.get(this.commandStack.size() - 1).run(
                 WaveBuilder.create()
                         .data(WaveData.build(UndoRedoWaves.UNDO_REDO, false))
                         .build()
@@ -77,18 +75,17 @@ public class UndoRedoService extends ServiceBase {
     public void undo() {
 
         // If there is at least one command left to undo
-        if (this.currentIndex > 0) {
-
+        if (this.commandStack.size() > 0) {
+            //Here put the last command on the undone stack
+            this.undoneStack.add(this.commandStack.get(this.commandStack.size() - 1));
+            //here remove the last command of the command stack
+            this.commandStack.remove(this.commandStack.get(this.commandStack.size() - 1));
             // Call Undo method
-            this.commandStack.get(this.currentIndex - 1).run(
+            this.undoneStack.get(this.undoneStack.size() - 1).run(
                     WaveBuilder.create()
                             .data(WaveData.build(UndoRedoWaves.UNDO_REDO, true))
                             .build()
-                    );
-
-            // Move backward the command cursor
-            this.currentIndex--;
-
+            );
         } else {
             // begin of stack, do nothing
             LOGGER.info("No more command to undo, begin of stack");
@@ -101,18 +98,17 @@ public class UndoRedoService extends ServiceBase {
     public void redo() {
 
         // If there is at least one command to redo
-        if (this.currentIndex < this.commandStack.size()) {
-
+        if (this.undoneStack.size() > 0) {
+            //add the last undone command on the command stack
+            this.commandStack.add(this.undoneStack.get(this.undoneStack.size() - 1));
+            //remove the command to redo of the undone stack.
+            this.undoneStack.remove(this.undoneStack.size() - 1);
             // Call Redo method
-            this.commandStack.get(this.currentIndex).run(
+            this.commandStack.get(this.commandStack.size() - 1).run(
                     WaveBuilder.create()
                             .data(WaveData.build(UndoRedoWaves.UNDO_REDO, false))
                             .build()
-                    );
-
-            // Move forward the command cursor
-            this.currentIndex++;
-
+            );
         } else {
             // End of stack, do nothing
             LOGGER.info("No more command to redo, end of stack");
