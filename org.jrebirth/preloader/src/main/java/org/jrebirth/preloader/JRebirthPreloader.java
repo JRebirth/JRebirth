47e33ed02b456020e2c3318abfc9ebd36b7236ee
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2013-10-09 20:56:28 +0200

    Add JRebirth Preloader to Analyzer Application

--- org.jrebirth/preloader/src/main/java/org/jrebirth/preloader/JRebirthPreloader.java
+++ org.jrebirth/preloader/src/main/java/org/jrebirth/preloader/JRebirthPreloader.java
@@ -1,6 +1,26 @@
+/**
+ * Get more info at : www.jrebirth.org .
+ * Copyright JRebirth.org © 2011-2013
+ * Contact : sebastien.bordes@jrebirth.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jrebirth.preloader;
 
+import javafx.animation.ScaleTransitionBuilder;
 import javafx.application.Preloader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,19 +28,28 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 /**
- * Hello world!
- * 
+ * Hello world!.
  */
 public class JRebirthPreloader extends Preloader
 {
+
+    /** The bar. */
     ProgressBar bar;
+
+    /** The stage. */
     Stage stage;
+
+    /** The text. */
     private Text text;
+
+    private boolean jrebirthInit = false;
 
     /**
      * {@inheritDoc}
@@ -30,6 +59,9 @@ public class JRebirthPreloader extends Preloader
         super.init();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void start(final Stage stage) throws Exception {
         this.stage = stage;
@@ -39,6 +71,11 @@ public class JRebirthPreloader extends Preloader
         stage.show();
     }
 
+    /**
+     * Creates the preloader scene.
+     * 
+     * @return the scene
+     */
     private Scene createPreloaderScene() {
 
         final StackPane p = new StackPane();
@@ -47,18 +84,18 @@ public class JRebirthPreloader extends Preloader
         p.getChildren().add(logo);
         StackPane.setAlignment(logo, Pos.CENTER);
 
-        this.bar = new ProgressBar();
+        this.bar = new ProgressBar(0.0);
         this.bar.setPrefSize(460, 20);
         p.getChildren().add(this.bar);
         StackPane.setAlignment(this.bar, Pos.BOTTOM_CENTER);
         StackPane.setMargin(this.bar, new Insets(30));
 
-        this.text = new Text("Boooooooooooooting up");
+        this.text = new Text("Loading");
         p.getChildren().add(this.text);
         StackPane.setAlignment(this.text, Pos.BOTTOM_CENTER);
         StackPane.setMargin(this.text, new Insets(10));
 
-        return new Scene(p, 600, 200);
+        return new Scene(p, 600, 200, Color.TRANSPARENT);
     }
 
     /**
@@ -66,13 +103,64 @@ public class JRebirthPreloader extends Preloader
      */
     @Override
     public void stop() throws Exception {
-        // Nothing to do yet
         super.stop();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void handleProgressNotification(final ProgressNotification pn) {
-        this.bar.setProgress(pn.getProgress());
+        if (jrebirthInit && pn.getProgress() >= 0.0 && pn.getProgress() <= 1.0) {
+            this.bar.setProgress(pn.getProgress());
+        } else {
+            this.text.setText(getMessageFromCode((int) pn.getProgress()));
+        }
+    }
+
+    /**
+     * Gets the message from code.
+     * 
+     * @param messageCode the message code
+     * @return the message from code
+     */
+    private String getMessageFromCode(int messageCode) {
+        String res = "";
+
+        switch (messageCode) {
+            case 100:
+                res = "Initializing";
+                break;
+            case 200:
+                res = "";// Provisioned for custom pre-init
+                break;
+            case 300:
+                res = "";// Provisioned for custom pre-init
+                break;
+            case 400:
+                res = "Loading Messages Properties";
+                break;
+            case 500:
+                res = "Loading Parameters Properties";
+                break;
+            case 600:
+                res = "Preparing Core Engine";
+                break;
+            case 700:
+                res = "Preloading Fonts";
+                break;
+            case 800:
+                res = "";// Provisioned for custom post-init
+                break;
+            case 900:
+                res = "";// Provisioned for custom post-init
+                break;
+            case 1000:
+                res = "Starting";
+                break;
+            default:
+        }
+        return res;
     }
 
     /**
@@ -82,6 +170,8 @@ public class JRebirthPreloader extends Preloader
     public void handleApplicationNotification(final PreloaderNotification n) {
         if (n instanceof MessageNotification) {
             this.text.setText(((MessageNotification) n).getMessage());
+        } else if (n instanceof ProgressNotification) {
+            handleProgressNotification((ProgressNotification) n);
         }
     }
 
@@ -92,14 +182,40 @@ public class JRebirthPreloader extends Preloader
     public void handleStateChangeNotification(final StateChangeNotification event) {
         switch (event.getType()) {
             case BEFORE_LOAD:
+                break;
             case BEFORE_INIT:
-                event.getApplication();
+                jrebirthInit = true;
                 break;
             case BEFORE_START:
-                this.stage.hide();
+                beforeStart();
                 break;
             default:
         }
+    }
+
+    /**
+     * TODO To complete.
+     * 
+     * @throws InterruptedException
+     */
+    private void beforeStart() {
+        final Stage stage = this.stage;
+
+        ScaleTransitionBuilder.create()
+                .fromX(1.0)
+                .toX(0.0)
+                .duration(Duration.millis(400))
+                .node(stage.getScene().getRoot())
+                .onFinished(new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent arg0) {
+                        stage.hide();
+
+                    }
+                })
+                .build().play();
+
     }
 
     /**
