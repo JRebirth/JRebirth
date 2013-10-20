427d401817f010cebbb639fa61134ceb862f018c
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2013-10-20 18:21:54 +0200

    Test

---
+++ org.jrebirth/core/src/test/java/org/jrebirth/core/concurrent/ThreadPoolApplication.java
@@ -0,0 +1,48 @@
+package org.jrebirth.core.concurrent;
+
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import org.jrebirth.core.application.Configuration;
+import org.jrebirth.core.application.DefaultApplication;
+import org.jrebirth.core.ui.Model;
+
+/**
+ * The class <strong>ThreadApplication</strong>.
+ * 
+ * @author Sébastien Bordes
+ */
+@Configuration(value = ".*-jrebirth", extension = "properties", schedule = 60)
+public class ThreadPoolApplication extends DefaultApplication<Pane> {
+
+    private static ThreadPoolApplication instance;
+
+    public static ThreadPoolApplication getInstance() {
+        return instance;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Class<? extends Model> getFirstModelClass() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void customizeStage(final Stage stage) {
+        instance = this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected String getApplicationTitle() {
+        return "Thread Application";
+    }
+
+}
