JRebirth/JRebirth

commit 3d6afbe9a9e4be31d31e5d836edef66e476e2b42

Add key robstness
new fxml feature
fxml junit test

share model object and registerCallback

org.jrebirth/core/src/main/java/org/jrebirth/core/link/AbstractReady.java
@@ -17,6 +17,9 @@
  */
 package org.jrebirth.core.link;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.jrebirth.core.command.Command;
 import org.jrebirth.core.exception.CoreException;
 import org.jrebirth.core.facade.EventType;
@@ -75,6 +78,42 @@ public abstract class AbstractReady<R extends FacadeReady<R>> implements FacadeR
     public void setKey(final UniqueKey key) {
         this.key = key;
     }
+
+    /**
+     * @return Returns the modelObject.
+     */
+    @SuppressWarnings("unchecked")
+    public Object getModelObject() {
+        Object modelObject = null;
+        if (this.key.getValue() instanceof List && !((List<Object>) this.key.getValue()).isEmpty()) {
+            modelObject = ((List<Object>) this.key.getValue()).get(0);
+        } else {
+            modelObject = this.key.getValue();
+        }
+        return modelObject;
+    }
+
+    /**
+     * @return Returns the list of model Object.
+     */
+    @SuppressWarnings("unchecked")
+    public List<Object> getListModelObject() {
+        List<Object> listModelObject = null;
+        if (this.key.getValue() instanceof List) {
+            listModelObject = (List<Object>) this.key.getValue();
+        } else {
+            listModelObject = Collections.emptyList();
+            listModelObject.add(this.key.getValue());
+        }
+        return listModelObject;
+    }
+
+    /**
+     * @param modelObject The modelObject to set.
+     */
+    // public void setModelObject(final Object modelObject) {
+    // this.modelObject = modelObject;
+    // }
 
     /**
      * {@inheritDoc}

org.jrebirth/analyzer/pom.xml
@@ -13,24 +13,6 @@
 
 	<build>
 
-		<resources>
-			<resource>
-				<filtering>false</filtering>
-				<directory>${basedir}/src/main/java</directory>
-				<includes>
-					<include>**/*.fxml</include>
-					<include>**/*.txt</include>
-				</includes>
-			</resource>
-			<resource>
-				<filtering>false</filtering>
-				<directory>${basedir}/src/main/resources</directory>
-				<includes>
-					<include>**/*.*</include>
-				</includes>
-			</resource>
-		</resources>
-
 		<plugins>
 
 			<plugin>

org.jrebirth/analyzer/src/main/java/org/jrebirth/analyzer/service/LoadEdtFileService.java
@@ -58,7 +58,7 @@ public class LoadEdtFileService extends ServiceBase {
     public void ready() throws CoreException {
         super.ready();
 
-        registerService(DO_LOAD_EVENTS, RE_EVENTS_LOADED/* , EditorWaves.EVENTS */);
+        registerCallback(DO_LOAD_EVENTS, RE_EVENTS_LOADED/* , EditorWaves.EVENTS */);
     }
 
     /**

org.jrebirth/core/.classpath
@@ -6,6 +6,7 @@
 			<attribute name="maven.pomderived" value="true"/>
 		</attributes>
 	</classpathentry>
+	<classpathentry kind="src" path="src/test/resources"/>
 	<classpathentry kind="src" output="target/test-classes" path="src/test/java">
 		<attributes>
 			<attribute name="optional" value="true"/>

org.jrebirth/core/pom.xml
@@ -19,6 +19,13 @@
 			<artifactId>slf4j-api</artifactId>
 			<version>1.7.2</version>
 		</dependency>
+
+		<dependency>
+			<groupId>ch.qos.logback</groupId>
+			<artifactId>logback-classic</artifactId>
+			<version>1.0.6</version>
+			<scope>test</scope>
+		</dependency>
 	</dependencies>
 
 </project>

org.jrebirth/core/src/main/java/org/jrebirth/core/concurrent/JRebirthThread.java
@@ -219,7 +219,11 @@ public final class JRebirthThread extends Thread {
         }
 
         // Manage the creation of the first node and show it !
-        chainedWaveList.add(getLaunchFirstViewWave());
+
+        final Wave firstViewWave = getLaunchFirstViewWave();
+        if (firstViewWave != null) {
+            chainedWaveList.add(firstViewWave);
+        }
 
         // Manage waves to run after the First node creation
         final List<Wave> postBootList = getApplication().getPostBootWaveList();
@@ -227,12 +231,14 @@ public final class JRebirthThread extends Thread {
             chainedWaveList.addAll(postBootList);
         }
 
-        getFacade().getNotifier().sendWave(
-                WaveBuilder.create()
-                        .waveGroup(WaveGroup.CALL_COMMAND)
-                        .relatedClass(ChainWaveCommand.class)
-                        .data(WaveData.build(JRebirthWaves.CHAINED_WAVES, chainedWaveList))
-                        .build());
+        if (!chainedWaveList.isEmpty()) {
+            getFacade().getNotifier().sendWave(
+                    WaveBuilder.create()
+                            .waveGroup(WaveGroup.CALL_COMMAND)
+                            .relatedClass(ChainWaveCommand.class)
+                            .data(WaveData.build(JRebirthWaves.CHAINED_WAVES, chainedWaveList))
+                            .build());
+        }
     }
 
     /**
@@ -286,12 +292,15 @@ public final class JRebirthThread extends Thread {
      * @return the wave responsible of the creation of the first view
      */
     protected Wave getLaunchFirstViewWave() {
-
+        Wave firstWave = null;
         // Generates the command cave directly to win a Wave turn
-        return ShowModelWaveBuilder.create()
-                .childrenPlaceHolder(this.application.getRootNode().getChildren())
-                .modelClass(this.application.getFirstModelClass())
-                .build();
+        if (this.application != null && this.application.getRootNode() != null && this.application.getFirstModelClass() != null) {
+            firstWave = ShowModelWaveBuilder.create()
+                    .childrenPlaceHolder(this.application.getRootNode().getChildren())
+                    .modelClass(this.application.getFirstModelClass())
+                    .build();
+        }
+        return firstWave;
 
     }
 

org.jrebirth/core/src/main/java/org/jrebirth/core/facade/AbstractFacade.java
@@ -28,7 +28,6 @@ import org.jrebirth.core.key.ClassKey;
 import org.jrebirth.core.key.KeyBuilder;
 import org.jrebirth.core.key.UniqueKey;
 import org.jrebirth.core.service.Service;
-import org.jrebirth.core.ui.AbstractModel;
 import org.jrebirth.core.ui.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -219,12 +218,6 @@ public abstract class AbstractFacade<R extends FacadeReady<R>> extends AbstractG
 
             // Create the unique key
             readyObject.setKey(buildKey((Class<R>) readyObject.getClass(), keyPart));
-
-            // FIXME IMPROVE IT
-            if (readyObject instanceof AbstractModel && keyPart.length > 0) {
-                // Attach the unique key (if any)
-                ((AbstractModel<?, ?>) readyObject).setModelObject(keyPart[0]);
-            }
 
             // Component Ready !
             return readyObject;

org.jrebirth/core/src/main/java/org/jrebirth/core/key/MultitonKey.java
@@ -121,6 +121,12 @@ public class MultitonKey<C> extends ClassKey<C> {
      */
     @Override
     public Object getValue() {
-        return this.keyPartList.get(0); // FIXME
+        Object obj = null;
+        if (this.keyPartList.size() > 1) {
+            obj = this.keyPartList;
+        } else if (this.keyPartList.size() == 1) {
+            obj = this.keyPartList.get(0);
+        }
+        return obj;
     }
 }

commit 234f7852759b4e3673fcfa6e521db0dd22ff5b05

Fix #90, bug introduced by 3d6afbe9a9e4be31d31e5d836edef66e476e2b42 commit Don't use Collection.emptyList() for mutable list

org.jrebirth/core/src/main/java/org/jrebirth/core/link/AbstractReady.java
@@ -17,6 +17,7 @@
  */
 package org.jrebirth.core.link;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -94,7 +95,12 @@ public abstract class AbstractReady<R extends FacadeReady<R>> implements FacadeR
     }
 
     /**
-     * @return Returns the list of model Object.
+     * Return the list of key parts (considered as model objects).
+     * 
+     * If the key value is an instance of List it will return it as is
+     * otherwise it will create a new list and add the unique key part object 
+     * 
+     * @return Returns a list composed by all model objects (= component key parts).
      */
     @SuppressWarnings("unchecked")
     public List<Object> getListModelObject() {
@@ -102,7 +108,7 @@ public abstract class AbstractReady<R extends FacadeReady<R>> implements FacadeR
         if (this.key.getValue() instanceof List) {
             listModelObject = (List<Object>) this.key.getValue();
         } else {
-            listModelObject = Collections.emptyList();
+            listModelObject = new ArrayList<>();
             listModelObject.add(this.key.getValue());
         }
         return listModelObject;