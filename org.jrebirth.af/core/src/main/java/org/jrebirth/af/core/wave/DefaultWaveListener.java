77b09f4cef30e4cbb5180cdab30336e9e950fe33
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2014-03-03 22:15:54 +0100

    Add default implementation of wave listener

---
+++ org.jrebirth.af/core/src/main/java/org/jrebirth/af/core/wave/DefaultWaveListener.java
@@ -0,0 +1,68 @@
+package org.jrebirth.af.core.wave;
+
+/**
+ * The class <strong>DefaultWaveListener</strong> is the default empty implementation.
+ * 
+ * You can use it as base class to only override the method you need.
+ * 
+ * @author Sébastien Bordes
+ */
+public class DefaultWaveListener implements WaveListener {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveCreated(Wave wave) {
+        // Nothing to do yet
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveSent(Wave wave) {
+        // Nothing to do yet
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveProcessed(Wave wave) {
+        // Nothing to do yet
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveCancelled(Wave wave) {
+        // Nothing to do yet
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveConsumed(Wave wave) {
+        // Nothing to do yet
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveFailed(Wave wave) {
+        // Nothing to do yet
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waveDestroyed(Wave wave) {
+        // Nothing to do yet
+    }
+
+}
