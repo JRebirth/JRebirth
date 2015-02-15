f6e470e16fc167d76ca9c1e63b0a123553613e5f
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2015-02-15 21:37:12 +0100

    Improve website (catcher zones)

---
+++ org.jrebirth.af/src/site/resources/js/carousel.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ 
+	$("#home_catcher").owlCarousel({
+	 
+		//navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		autoPlay: 10000,
+		singleItem:true,
+		autoPlay:true
+		 
+		// "singleItem:true" is a shortcut for:
+		// items : 1,
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+	 
+	});
+ 
+});
+
---
+++ org.jrebirth.af/src/site/resources/js/scrollmenu.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	$('#page').scrollNav(
+	/*{
+	    sections: 'h2',
+	    subSections: true,
+	    sectionElem: 'section',
+	    showHeadline: true,
+	    headlineText: 'Scroll To',
+	    showTopLink: true,
+	    topLinkText: 'Top',
+	    fixedMargin: 40,
+	    scrollOffset: 40,
+	    animated: true,
+	    speed: 500,
+	    insertTarget: this.selector,
+	    insertLocation: 'insertBefore',
+	    arrowKeys: false,
+	    scrollToHash: true
+	}*/
+	);
+
+}
