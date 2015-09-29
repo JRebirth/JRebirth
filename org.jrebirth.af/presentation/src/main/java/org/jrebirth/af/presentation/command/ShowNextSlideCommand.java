f30e75f951dff36e717c72c0261fd3e3d5c96885
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2015-09-29 22:54:49 +0200

    Manage SkipSlideStep for SlideStackModel

--- org.jrebirth.af/presentation/src/main/java/org/jrebirth/af/presentation/command/ShowNextSlideCommand.java
+++ org.jrebirth.af/presentation/src/main/java/org/jrebirth/af/presentation/command/ShowNextSlideCommand.java
@@ -19,6 +19,7 @@ package org.jrebirth.af.presentation.command;
 
 import org.jrebirth.af.api.wave.Wave;
 import org.jrebirth.af.core.command.single.ui.DefaultUICommand;
+import org.jrebirth.af.presentation.PrezWaves;
 import org.jrebirth.af.presentation.ui.stack.SlideStackModel;
 
 /**
@@ -34,7 +35,7 @@ public final class ShowNextSlideCommand extends DefaultUICommand {
      */
     @Override
     protected void perform(final Wave wave) {
-        getModel(SlideStackModel.class).next();
+        getModel(SlideStackModel.class).next(wave.get(PrezWaves.SKIP_SLIDE_STEP));
     }
 
 }
