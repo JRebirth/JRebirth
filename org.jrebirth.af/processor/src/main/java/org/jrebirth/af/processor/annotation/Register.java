c5802d48317ed6f0600e3e2bd2a4131a6d32622b
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2015-10-14 21:59:02 +0200

    Improve javadoc for modules

--- org.jrebirth.af/processor/src/main/java/org/jrebirth/af/processor/annotation/Register.java
+++ org.jrebirth.af/processor/src/main/java/org/jrebirth/af/processor/annotation/Register.java
@@ -23,7 +23,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Interface Register.
  */
