fe86fcd5b45ff303f21cd24121716b458bc36004
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2015-11-25 20:39:05 +0100

    Add Annoation for Generic Event handler

---
+++ org.jrebirth.af/api/src/main/java/org/jrebirth/af/api/ui/annotation/type/Event.java
@@ -0,0 +1,51 @@
+/**
+ * Get more info at : www.jrebirth.org .
+ * Copyright JRebirth.org © 2011-2014
+ * Contact : sebastien.bordes@jrebirth.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF Any KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jrebirth.af.api.ui.annotation.type;
+
+import javafx.event.EventType;
+
+/**
+ * The Generic event type.<br />
+ * The event type will be appended to method name to use.
+ */
+public enum Event implements EnumEventType {
+
+    /** Generic event type. */
+    Any(javafx.event.Event.ANY);
+
+    /** The JavaFX internal api name. */
+    private EventType<?> eventType;
+
+    /**
+     * Default constructor used to link the apiName.
+     *
+     * @param eventType the javafx event type
+     */
+    private Event(final EventType<?> eventType) {
+        this.eventType = eventType;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EventType<?> eventType() {
+        return this.eventType;
+    }
+
+}
