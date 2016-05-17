f8e0074f495164d382fb3efa6d337e200fd22198
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2016-05-17 08:05:26 +0200

    Add workbench demo

---
+++ org.jrebirth.af/showcase/workbench/src/main/java/org/jrebirth/af/demo/workbench/WorkbenchApplication.java
@@ -0,0 +1,42 @@
+package org.jrebirth.af.demo.workbench;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+import org.jrebirth.af.api.ui.Model;
+import org.jrebirth.af.component.resources.ComponentStyles;
+import org.jrebirth.af.core.application.DefaultApplication;
+import org.jrebirth.af.demo.workbench.ui.TabDemoModel;
+
+/**
+ * The class <strong>WorkbenchApplication</strong>.
+ *
+ * @author
+ */
+public final class WorkbenchApplication extends DefaultApplication<StackPane> {
+
+    /**
+     * Application launcher.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(final String... args) {
+        launch(WorkbenchApplication.class, args);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Class<? extends Model> firstModelClass() {
+        return TabDemoModel.class;
+    }
+
+    @Override
+    protected void customizeScene(final Scene scene) {
+        super.customizeScene(scene);
+
+        addCSS(scene, ComponentStyles.DEFAULT);
+    }
+
+}
---
+++ org.jrebirth.af/showcase/workbench/src/main/java/org/jrebirth/af/demo/workbench/ui/DockableModel.java
@@ -0,0 +1,24 @@
+package org.jrebirth.af.demo.workbench.ui;
+
+import javafx.scene.text.Text;
+
+import org.jrebirth.af.component.behavior.dockable.data.Dockable;
+import org.jrebirth.af.core.ui.simple.DefaultSimpleObjectModel;
+
+public class DockableModel extends DefaultSimpleObjectModel<Text, String> {
+
+    @Override
+    protected void initModel() {
+        addBehavior(Dockable.create()
+                            .name(object())
+                            .modelKey(key()));
+    }
+
+    @Override
+    protected void initSimpleView() {
+
+        node().setText(object());
+
+    }
+
+}
