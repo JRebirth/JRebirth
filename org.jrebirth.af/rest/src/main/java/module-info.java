87a52968cb3de68f5acad5be9f5376a333244b5c
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2019-08-29 00:35:48 +0200

    Fix Rest ModuleStarter

--- org.jrebirth.af/rest/src/main/java/module-info.java
+++ org.jrebirth.af/rest/src/main/java/module-info.java
@@ -8,10 +8,15 @@ module org.jrebirth.af.rest {
     exports org.jrebirth.af.rest;
     exports org.jrebirth.af.rest.service;
     exports org.jrebirth.af.rest.annotation;
+    
+    opens org.jrebirth.af.rest.service to org.jrebirth.af.core;
+    opens org.jrebirth.af.rest.service.impl to org.jrebirth.af.core;
 
     requires java.ws.rs;
     requires javafx.base;
     requires org.jrebirth.af.api;
     requires org.jrebirth.af.core;
     requires org.slf4j;
+    
+    provides org.jrebirth.af.api.module.ModuleStarter with org.jrebirth.af.rest.RestModuleStarter;
 }
