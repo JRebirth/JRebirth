fc8e71da0a3ac9532eaa485112bf2669b6b193f3
Sébastien Bordes <sebastien.bordes@jrebirth.org>
2026-05-02 00:13:25 +0200

    Clean demo for fonticon

--- org.jrebirth.af/showcase/fonticon/src/main/java/org/jrebirth/af/showcase/fonticon/ui/main/MainModel.java
+++ org.jrebirth.af/showcase/fonticon/src/main/java/org/jrebirth/af/showcase/fonticon/ui/main/MainModel.java
@@ -3,9 +3,6 @@ package org.jrebirth.af.showcase.fonticon.ui.main;
 import java.util.Collections;
 import java.util.stream.Stream;
 
-import javafx.scene.layout.BorderPane;
-import javafx.util.Duration;
-
 import org.jrebirth.af.api.module.Register;
 import org.jrebirth.af.api.ui.ModuleModel;
 import org.jrebirth.af.api.wave.contract.WaveData;
@@ -23,10 +20,11 @@ import org.jrebirth.af.iconfontbridge.typicons.Typicons;
 import org.jrebirth.af.iconfontbridge.weathericons.WeatherIcons;
 import org.jrebirth.af.showcase.fonticon.ui.fonticon.FontIconModel;
 
+import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
+
 @Register(value = ModuleModel.class)
 public class MainModel extends DefaultSimpleModel<BorderPane> implements ModuleModel {
-
-    private TabbedPaneModel tabModel;
 
     /**
      * {@inheritDoc}
@@ -41,10 +39,8 @@ public class MainModel extends DefaultSimpleModel<BorderPane> implements ModuleM
      */
     @Override
     protected void initSimpleView() {
-        // node().setCenter(tabModel.node());
 
-        // if (!this.hasShown.getAndSet(true)) {
-        final FadeInOutWaveBean fiowb = FadeInOutWaveBean.create().showDuration(Duration.millis(4000));
+    	final FadeInOutWaveBean fiowb = FadeInOutWaveBean.create().showDuration(Duration.millis(4000));
 
         final Stream<Dockable> tabs = Stream.of(FontAwesome.class, EmojiOne.class, Icons525.class, WeatherIcons.class, Typicons.class)
                                             .map(c -> Dockable.create()
@@ -56,7 +52,6 @@ public class MainModel extends DefaultSimpleModel<BorderPane> implements ModuleM
                                                      .tabs(tabs.toArray(Dockable[]::new));
 
         attachUi(TabbedPaneModel.class, WBuilder.buildUiData(node().centerProperty(), fiowb, Collections.singletonList(cfg)).toArray(new WaveData[0]));
-        // }
     }
 
     @Override
